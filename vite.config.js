889019409e5fde5cb8e6fdc66b8b2a4b85734ecd
devtwocyphernate <ontimeway001@gmail.com>
2024-12-20 12:18:34 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    vite: {
-        server: {
-            watch: {
-                usePolling: true,
-            },
-        },
-    },
+  plugins: [react()],
+  base: './', // Use this if your app is in a subfolder
+  build: {
+    outDir: 'dist', // Default is 'dist', ensure this matches
+  },
+
 });
